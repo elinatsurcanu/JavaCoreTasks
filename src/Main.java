c970ed23c3ceec98277ebb77692a1d6b98d023ae
elinatsurcanu <eleatsurcanu@gmail.com>
2022-09-06 14:45:20 +0300

    Added Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void concatenateValues(Object array[])
+    {
+        String str = "";
+        int sum = 0;
+        ArrayList<Person> people = new ArrayList<Person>();
+        for(int i = 0; i < array.length; i++)
+
+        {
+            if(array[i] instanceof String || array[i] instanceof Character) str += array[i] + " ";
+            if(array[i] instanceof Integer) sum += (int) array[i];
+            if(array[i] instanceof Person)
+            {
+                people.add((Person) array[i]);
+            }
+        }
+        System.out.println("Concatenation of all strings and characters: " + str);
+        System.out.println("Sum of all numbers: " + sum);
+        System.out.println("Array with people only:");
+        for(Person p: people)
+            p.print();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Object[] array = new Object[]{"Today", new Person(), 334, "is", 23, 652, 'a', new Person("Ann", "Jenner", 22, 'F'), "big", "day"};
+        concatenateValues(array);
     }
 }
